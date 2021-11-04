2a8a0475fbf0469f355f4b150de9cefa4929a1ae
cjhoon96 <cjhoon96@gmail.com>
2021-11-04 10:34:24 +0900

    SAP

--- db_test.php
+++ db_test.php
@@ -1,10 +1,37 @@
 <?php
+// 데이터베이스 접속
 $conn = mysqli_connect("localhost", "root", "", "temp_db");
-$search_result = mysqli_query($connect_no, "SLELCT * FROM login_t");
-$how_many = mysqli_num_rows($search_result);
-for($loop=1; $loop <= $how_many; $loop++){
-    $record = mysqli_fetch_array($search_result);
-    echo("name:" . $record["name"]);
-    echo("address:" . $record["address"] . "<hr>");
-}
+
+
+//데이터베이스로 하고 싶은 작업
+// 우리가 만든 데이터베이스의 테이블에 뭔가를 저장하고 싶다.
+// temp_db 안에 temp_t 테이블이 있다. 여기에 데이터를 저장한다.
+// 테이블 이름수정 --> 테이블의 칸(컬럼or필드)의 이름이 틀림
+// 테이블 칸 수가 안맞음
+
+// $search_result = mysqli_query($connect_no, "SLELCT * FROM login_t");
+
+$result = mysqli_query($conn, 
+                        "INSERT INTO temp_t (u_name, u_addr, u_phone)
+                        VALUES ('superuser', 'SEOUL', '01099999999')" );
+$result = mysqli_query($conn, 
+                        "INSERT INTO temp_t (u_name, u_addr, u_phone)
+                        VALUES ('superman', 'SEOUL', '01099999999')" );                        
+
+
+
+// $result = mysqli_query($conn, 
+//                         "INSERT INTO info (u_nema, u_addr, u_email, u_phone, u_personalID, u_travel)
+//                         VALUES ($u_nema, $u_addr, $u_email, $u_phone, $u_personalID, $u_travel)" )
+
+// $how_many = mysqli_num_rows($search_result);
+
+// for($loop=1; $loop <= $how_many; $loop++){
+//     $record = mysqli_fetch_array($search_result);
+//     echo("name:" . $record["name"]);
+//     echo("address:" . $record["address"] . "<hr>");
+// }
+
+mysqli_close($conn);
+
 ?>
--- member_reg.php
+++ member_reg.php
@@ -1,4 +1,4 @@
-<html>
+html>
     <head>
         <meta charset="utf-8">
 				<link rel="stylesheet" href="style.css">
@@ -14,12 +14,30 @@
         
         $u_name = $_GET["u_name_box"];      //get에서 "u_name_box" 데이터만 만 꺼내라
         $u_addr = $_GET["u_addr_box"];
-        $u_email = $_GET["u_email_box"];
         $u_phone = $_GET["u_phone_box"];
-        $u_personalID = $_GET["u_personalID_box"];
-				$u_travel = $_GET["u_travel_box"];
-				$data = array($u_name, $u_addr, $u_email, $u_phone, $u_personalID, $u_travel)
 
+        // $u_email = $_GET["u_email_box"];
+        // $u_personalID = $_GET["u_personalID_box"];
+        // $u_travel = $_GET["u_travel_box"];
+        // $data = array($u_name, $u_addr, $u_email, $u_phone, $u_personalID, $u_travel)
+
+
+        // 데이터베이스 접속
+        $conn = mysqli_connect("localhost", "root", "", "temp_db");
+
+        $result = mysqli_query($conn, 
+                                "INSERT INTO temp_t (u_name, u_addr, u_phone)
+                                VALUES ('{$u_name}', '{$u_addr}', '{$u_phone}')" );
+
+        // $how_many = mysqli_num_rows($search_result);
+
+        // for($loop=1; $loop <= $how_many; $loop++){
+        //     $record = mysqli_fetch_array($search_result);
+        //     echo("name:" . $record["name"]);
+        //     echo("address:" . $record["address"] . "<hr>");
+        // }
+
+        mysqli_close($conn);
 		
         
         ?>
@@ -36,20 +54,20 @@
           </tr>
 
           <tr>
-            <td>u_addr</td>
-            <td><?=$u_addr?></td>
-          </tr>
-          
-          <tr>
-            <td>u_email</td>
-            <td><?=$u_email?></td>
-          </tr>
-
-          <tr>
             <td>u_phone</td>
             <td><?=$u_phone?></td>
           </tr>
           
+          <tr>
+            <td>u_addr</td>
+            <td><?=$u_addr?></td>
+          </tr>
+          
+          <!-- <tr>
+            <td>u_email</td>
+            <td><?=$u_email?></td>
+          </tr>
+
           <tr>
             <td>u_personalID</td>
             <td><?=$u_personalID?></td>
@@ -58,7 +76,7 @@
           <tr>
             <td>u_travel</td>
             <td><?=$u_travel?></td>
-          </tr>
+          </tr> -->
         </table>
     </body>
 </html>
